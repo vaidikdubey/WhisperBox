c712ebbee9d17471c7ab9b882f1f2923854eb10c
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-08 22:21:53 +0530

    Added msg and user models

---
+++ src/model/User.ts
@@ -0,0 +1,73 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+//Using extends document since all this data will eventually make mongoose document
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+});
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"], //The second value is the custom error msg
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/.+\@.+\..+/, "Please use a valid email address"], //Regex validation of email
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify code is required"],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify code expiry is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true,
+    },
+    messages: [MessageSchema],
+});
+
+//Since Next.js runs on Edge servers, we need to take in account the fact that the server might not know if this is the first time of app and need to create schema or if schema is already present. So we create models to incorporate both these things.
+const UserModel =
+    (mongoose.models.User as mongoose.Model<User>) || //Already created model
+    mongoose.model<User>("User", UserSchema); //First time run, create new model
+
+export default UserModel;
