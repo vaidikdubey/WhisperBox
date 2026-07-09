f4f55803dd9f9f0345affdf45ff7fe5c4a893f64
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-09 14:08:45 +0530

    Added verify code email functionality

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,29 @@
+import { resend } from "../lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string,
+): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from: "Acme <onboarding@resend.dev>",
+            to: email,
+            subject: "WhisperBox - Verification Code",
+            react: VerificationEmail({ username, otp: verifyCode }),
+        });
+
+        return {
+            success: true,
+            message: "Verification email sent successfully",
+        };
+    } catch (emailError) {
+        console.error("Error sending verificaton email: ", emailError);
+        return {
+            success: false,
+            message: "Failed to send verification email",
+        };
+    }
+}
