d0c1007c48dbf0d83127d50018ec811835f27a89
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-09 12:58:31 +0530

    Added db conection method

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+//Using void here to specify that we don't care about the type of data being retured in promise
+async function dbConnect(): Promise<void> {
+    //Since next works on edge, we always check first if we have an already existing connection to db to avoid db choking
+    if (connection.isConnected) {
+        console.log("Already connected to database ⏱️");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+
+        console.log("DB: ", db);
+
+        connection.isConnected = db.connections[0].readyState;
+
+        console.log("DB connected ✅");
+    } catch (error) {
+        console.error("Database connection failed ❌", error);
+        process.exit(1);
+    }
+}
+
+export default dbConnect;
