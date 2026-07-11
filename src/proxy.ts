49506f8be3f4c4a51455ea9a81d0a08dc9b0608a
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-11 21:36:30 +0530

    Added testing page for route. Updated proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware"; //Apply middleware everywhere
 import { getToken } from "next-auth/jwt";
 
-// This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
     const token = await getToken({ req: request });
     const url = request.nextUrl;
@@ -18,7 +17,11 @@ export async function proxy(request: NextRequest) {
         return NextResponse.redirect(new URL("/dashboard", request.url));
     }
 
-    return NextResponse.redirect(new URL("/home", request.url));
+    if (!token && url.pathname.startsWith("/dashboard")) {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
+    return NextResponse.next();
 }
 
 export const config = {
