2ceb72b0b20b0505c056f2e58aac535e9cfa6eb1
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-14 21:59:00 +0530

    Added suggest message route

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,35 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  try {
+    const prompt =
+      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+    const result = streamText({
+      model: openai('gpt-3.5-turbo-instruct'),
+        providerOptions: {
+            openai: {
+                maxTokens: 400,
+            }
+        },
+      prompt: prompt,
+    });
+
+    // This handles headers and streams the text data response safely
+    return result.toTextStreamResponse();
+
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      const { name, status, headers, message } = error;
+      return NextResponse.json({ name, status, headers, message }, { status });
+    } else {
+      console.error('An unexpected error occurred:', error);
+      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+  }
+}
