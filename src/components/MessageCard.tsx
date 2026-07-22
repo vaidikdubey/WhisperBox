e5b44983f3712e51874d3ed7507cefc9cf587a3b
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-22 21:20:03 +0530

    Added Message Card component

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,6 +1,76 @@
-const MessageCard = () => {
-  return (
-    <div>MessageCard</div>
-  )
+"use client";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+import { Message } from "@/model/User";
+import axios from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import { toast } from "sonner";
+import {Types} from "mongoose"
+
+type MessageCardProps = {
+  message: Message,
+  onMessageDelete: (messageId: Types.ObjectId) => void
 }
-export default MessageCard
+
+const MessageCard = ({ message, onMessageDelete}: MessageCardProps) => {
+  const handleDeleteConfirm = async () => {
+    const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+
+    toast.success("Success", {
+      description: response.data.message
+    })
+
+    onMessageDelete(message._id)
+  }
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <AlertDialog>
+                    <AlertDialogTrigger
+                        render={<Button variant="destructive"><X className="w-5 h-5" /></Button>}
+                    />
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>
+                                Are you absolutely sure?
+                            </AlertDialogTitle>
+                            <AlertDialogDescription>
+                                This action cannot be undone. This will
+                                permanently delete this message from our
+                                servers.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+                <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent></CardContent>
+        </Card>
+    );
+};
+export default MessageCard;
