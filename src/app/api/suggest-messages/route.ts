1ef648805c9ab3b0f2e3c455c59476d956e9706f
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-23 21:55:58 +0530

    Added suggest message AI feature in public page component. Updated suggest message route to support streaming response.

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,6 +1,10 @@
-import { google } from "@ai-sdk/google";
-import { streamText, createTextStreamResponse, toTextStream } from "ai";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { streamText, createUIMessageStreamResponse, toUIMessageStream } from "ai";
 import { NextResponse } from "next/server";
+
+const google = createGoogleGenerativeAI({
+    apiKey: process.env.GEMINI_API_KEY
+})
 
 export const runtime = "edge";
 
@@ -10,15 +14,14 @@ export async function POST(req: Request) {
             "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
         const result = streamText({
-            model: google("gemini-2.5-flash"),
-            maxOutputTokens: 400,
+            model: google("gemini-3.6-flash"),
             prompt: prompt,
         });
 
         // This handles headers and streams the text data response safely
-        return createTextStreamResponse({
-            stream: toTextStream({ stream: result.stream }),
-        });
+        return createUIMessageStreamResponse({
+            stream: toUIMessageStream({stream: result.stream})
+        })
     } catch (error) {
         console.error("An unexpected error occurred:", error);
         return NextResponse.json(
--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Separator } from "@/components/ui/separator";
 import { Textarea } from "@/components/ui/textarea";
 import { messageSchema } from "@/schemas/messageSchema";
 import { ApiResponse } from "@/types/ApiResponse";
+import { useCompletion } from "@ai-sdk/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios, { AxiosError } from "axios";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { toast } from "sonner";
 import * as z from "zod";
+
+const specialChar = "||"; //For future updates
+
+const initialMessage =
+    "If you could instantly become an expert in any skill or topic overnight, what would you choose?||What’s a small, unexpected thing that made you smile recently?||If you could design a dream vacation with no budget limits, where would we be heading?";
 
 const Page = () => {
     const params = useParams<{ username: string }>();
@@ -26,6 +35,8 @@ const Page = () => {
         },
     });
 
+    const messageContent = form.watch("content");
+
     const onSubmit = async (data: z.infer<typeof messageSchema>) => {
         setIsSubmitting(true);
 
@@ -38,6 +49,8 @@ const Page = () => {
             toast.success("Success", {
                 description: response.data.message,
             });
+
+            form.reset({ ...form.getValues(), content: "" });
         } catch (error) {
             const axiosError = error as AxiosError<ApiResponse>;
 
@@ -53,8 +66,41 @@ const Page = () => {
         }
     };
 
+    const {
+        complete,
+        completion,
+        isLoading: isSuggestLoading,
+        error,
+    } = useCompletion({
+        api: "/api/suggest-messages",
+        initialCompletion: initialMessage,
+    });
+
+    const fetchAISuggestionMessages = async () => {
+        try {
+            await complete(" ");
+
+            toast.success("Success", {
+                description: "Messages generated",
+            });
+        } catch (error) {
+            console.error("Error generating message ", error);
+            toast.error("Error", {
+                description: "Error generating messages",
+            });
+        }
+    };
+
+    const parseStringMessages = (message: string): string[] => {
+        return message.split(specialChar);
+    };
+
+    const handleMessageClick = (message: string) => {
+        form.setValue("content", message);
+    };
+
     return (
-        <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+        <div className="my-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
             <h1 className="text-4xl font-bold mb-4 text-center">
                 Public Profile Link
             </h1>
@@ -70,7 +116,10 @@ const Page = () => {
                         render={({ field, fieldState }) => (
                             <Field data-invalid={fieldState.invalid}>
                                 <FieldLabel htmlFor="send-message-form-content">
-                                    Send Anonymous Message to @{params.username}
+                                    Send Anonymous Message to
+                                    <span className="font-bold">
+                                        @{params.username}
+                                    </span>
                                 </FieldLabel>
                                 <Textarea
                                     {...field}
@@ -92,18 +141,65 @@ const Page = () => {
                     variant="secondary"
                     type="submit"
                     form="send-message-form"
-                    disabled={isSubmitting}
-                    className="w-fit mx-auto mt-5"
+                    disabled={isSubmitting || !messageContent}
+                    className="w-fit mx-auto my-5"
                 >
                     Send It
                 </Button>
             </div>
 
-            <Button>Suggest Messages</Button>
+            <Button
+                onClick={fetchAISuggestionMessages}
+                disabled={isSuggestLoading}
+            >
+                {isSuggestLoading ? (
+                    <>
+                        {" "}
+                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />{" "}
+                        Getting Suggestions{" "}
+                    </>
+                ) : (
+                    "Suggest Messages"
+                )}
+            </Button>
             <Separator className="my-4" />
 
             {/* AI Generated Messages */}
-            <div></div>
+            <div className="flex flex-col gap-2">
+                <h3>Click on any message below to select it.</h3>
+                <Card>
+                    <CardHeader>
+                        <h3 className="text-xl font-semibold">Messages</h3>
+                    </CardHeader>
+                    <CardContent className="flex flex-col space-y-4">
+                        {error ? (
+                            <p className="text-red-500">{error.message}</p>
+                        ) : (
+                            parseStringMessages(completion).map(
+                                (message, index) => (
+                                    <Button
+                                        key={index}
+                                        variant="outline"
+                                        className="mb-2 whitespace-normal h-fit p-3"
+                                        onClick={() =>
+                                            handleMessageClick(message)
+                                        }
+                                    >
+                                        {message}
+                                    </Button>
+                                ),
+                            )
+                        )}
+                    </CardContent>
+                </Card>
+            </div>
+            <Separator className="my-6" />
+            <div className="text-center">
+                <div className="mb-4">Start Your Anonymous Adventure Today</div>
+                <Link href={"/sign-up"}>
+                    <Button>Join WhisperBox</Button>
+                </Link>
+            </div>
         </div>
     );
 };
